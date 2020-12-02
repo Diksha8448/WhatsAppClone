ee7e63fc541c4f7f57806996fe454b3ee6a7964b
Diksha  Rajput <67539159+Diksha8448@users.noreply.github.com>
2020-12-02 15:04:49 +0530

    Components added

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets Build a MERN WhatsApp CLone</h1>
+      <Sidebar/>
+      <Chat/>
+      {/*Chat Component */}
     </div>
   );
 }
---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABkm7NAIeSvPrL78wl1A9WhszjzPWmDe0",
+    authDomain: "whatsapp-clone-eae78.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-eae78.firebaseio.com",
+    projectId: "whatsapp-clone-eae78",
+    storageBucket: "whatsapp-clone-eae78.appspot.com",
+    messagingSenderId: "1051730625288",
+    appId: "1:1051730625288:web:bdcb4434915713858f22bd",
+    measurementId: "G-56L7S368B0"
+  };
+  
