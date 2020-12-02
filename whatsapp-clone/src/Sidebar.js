d8e680b013d1e536a50d1df209fa22d4bf286eea
Diksha  Rajput <67539159+Diksha8448@users.noreply.github.com>
2020-12-02 16:54:17 +0530

    Search bar

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,15 +1,36 @@
 import React from "react";
 import "./Sidebar.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { IconButton } from "@material-ui/core";
+import {Avatar} from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {SearchOutlined} from "@material-ui/icons";
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar_header">
+                <Avatar src="https://avatarmaker.net/images/7.png"/>
                 <div className="sidebar_headerRight">
-                    <DonutLargeIcon/>
+                    <IconButton>
+                        <DonutLargeIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon/>
+                    </IconButton>
+                    
                 </div>
 
+            </div>
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or start new chat" type="text"/>
+                </div> 
             </div>
         </div>
     );
