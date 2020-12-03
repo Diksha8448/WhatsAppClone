6e1a8b2d2a5141ed3277a641c4d4994669a88e3f
Diksha  Rajput <67539159+Diksha8448@users.noreply.github.com>
2020-12-03 16:18:47 +0530

    server.js

---
+++ whatsapp-backend/server.js
@@ -0,0 +1,14 @@
+//importing
+import express from 'express' 
+
+//app config
+const app=express()
+//middleware
+
+//DB config
+
+//????
+
+//api routes
+app.get('/',(req,res)=>res.status(200).send('hello world'))
+//listener
