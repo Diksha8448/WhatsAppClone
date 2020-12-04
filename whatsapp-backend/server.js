438bd37cbbc14eb07a09fcbc876aa9f265436685
Diksha  Rajput <67539159+Diksha8448@users.noreply.github.com>
2020-12-04 15:05:32 +0530

    server file

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,14 +1,23 @@
 //importing
-import express from 'express' 
+import express from "express";
+import mongoose from "mongoose";
 
 //app config
-const app=express()
+const app=express();
+const port=process.env.PORT || 9000;
 //middleware
 
 //DB config
+const connection_url="mongodb+srv://admin:CUIxCYqjzD6P78md@cluster0.xvvju.mongodb.net/whatsappdb?retryWrites=true&w=majority";
+mongoose.connect(connection_url,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
 
 //????
 
 //api routes
-app.get('/',(req,res)=>res.status(200).send('hello world'))
+app.get("/",(req,res)=>res.status(200).send("hello world"));
 //listener
+app.listen(port,()=>console.log(`Listening on localhost:${port}`));
